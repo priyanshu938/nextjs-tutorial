25952c1d9a393de8435a36478d204e766fe59dab
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2022-08-12 21:56:22 +0530

    links in next js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
 const index = () => {
-  return <div>hello World</div>;
+  return (
+    <div>
+      <navbar>
+        <ul>
+          {
+            // use Link for routing
+          }
+          <Link href="/home">Home</Link>
+          <Link href="/contact">Contact</Link>
+          <Link href="/about">About</Link>
+          <Link href="/blog">Blog</Link>
+        </ul>
+      </navbar>
+    </div>
+  );
 };
 
 export default index;
