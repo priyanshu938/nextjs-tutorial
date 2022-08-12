3eaddc652b46510119fd26488353d4b4e1a17790
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2022-08-12 23:34:13 +0530

    add css in next js

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-
+import styles from "../styles/Home.module.css";
 const home = () => {
   return (
     <div>
-     <Navbar />
-     home
+      <Navbar />
+      {
+        //we can apply multiple classes on one component like below
+      }
+      <div className={`${styles.home} ${styles.homeBg}`}>home</div>
     </div>
   );
 };
